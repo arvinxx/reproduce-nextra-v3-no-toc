c3a5f3474d60a051114439236a78ceb5d288b410
arvinxx <arvinx@foxmail.com>
2024-03-10 00:02:54 +0800

    init

--- src/utils/joinWithBaseUrl.ts
+++
@@ -1,5 +0,0 @@
-import urlJoin from 'url-join';
-
-export const joinWithBaseUrl = (path: string): string => {
-  return urlJoin(process.env.NEXT_PUBLIC_BASE_PATH || '/', path);
-};
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,20 +2,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs';
 
 
 const config: DocsThemeConfig = {
-  chat: {
-    link: 'https://discord.gg/AYFPHvv2jT',
-  },
-  docsRepositoryBase: 'https://github.com/lobehub/lobe-chat/tree/main/docs',
-  footer: {
-    content: `MIT 2023-${new Date().getFullYear()} © LobeHub, LLC`,
-  },
   i18n: [
     { locale: 'en', name: 'English' },
     { locale: 'zh', name: '中文' },
-  ],
-  project: {
-    link: 'https://github.com/lobehub/lobe-chat',
-  },
+  ]
 };
 
 export default config;
